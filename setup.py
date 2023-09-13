4e4f7f5987da880e33e5e8f7d3a5a7f855e68995
Benedikt Volkmer <benedikt.volkmer@nuromedia.com>
2023-09-13 15:31:10 +0200

    Remove . from requirements and add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
-from setuptools import setup, find_packages
- 
-setup(name = "library", packages = find_packages())
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+setup(
+    name="library",
+    packages=find_packages(),
+    install_requires=list(
+        filter(
+            lambda x: not x.startswith("#"),
+            Path("./requirements.txt").read_text().splitlines(),
+        )
+    ),
+)
+
